1c2f0ffb031ac09b33e7da80d08a97384947a34b
Abigail Wezelis <aaw38@pitt.edu>
2018-10-21 17:29:53 -0400

    move game and player classes to separate files

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Game.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public int NumPlayers { get; set; }
+        public Player[] Players { get; set; }
+        public int CurrPlayer { get; set; }
+
+        public Game(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Player.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class Player
+    {
+        public string playerName { get; set; }
+        public string playerColor { get; set; }
+        public uint boardPosition { get; set; }
+        public int numLoans { get; set; }
+        public int numFriends { get; set; }
+        public int numCredits { get; set; }
+        public bool isCommunityCollege { get; set; }
+        public bool isGraduated { get; set; }
+    }
+}
