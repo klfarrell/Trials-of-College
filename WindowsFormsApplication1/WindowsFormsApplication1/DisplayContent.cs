7ddb47f188cfde0f346e309ddb46d6ef2266574d
matthew0257 <mjopdyke@gmail.com>
2018-11-08 17:12:12 -0500

    Work in progress: UI implementation, committing this so I can pull and push real quick

--- WindowsFormsApplication1/WindowsFormsApplication1/DisplayContent.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/DisplayContent.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
     public class DisplayContent
     {
+        private List<Control> _controls;
 
+        public DisplayContent() {
+            _controls = new List<Control>();
+        }
+
+        public void AddControl(Control control)
+        {
+            _controls.Add(control);
+        }
+
+        public List<Control> Controls
+        {
+            get
+            {
+                return _controls;
+            }
+        }
     }
 }
